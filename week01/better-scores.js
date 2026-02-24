d038e43e408804255dbe237842f488af0dd12277
Alverino Stefano s354116 <s312200@studenti.polito.it>
2026-02-24 17:14:14 +0100

    feat: basics-execises done

--- week01/better-scores.js
+++ week01/better-scores.js
@@ -16,49 +16,67 @@ Develop a small JavaScript program to manage the scores given to your user on we
 */
 
 
-const scores = [3, -2, 10, 0, -5, -8, 7, 4, -1, 9, 6] ;
+const scores = [3, -2, 10, 0, -5, 7, 4, -1, 9, 6] ;
 
-const newscores = []
-
-for (const value of scores) {
-  if (value >= 0) {
-    newscores.push(value)
+const new_scores = [...scores] //copy of scores
+console.log(new_scores)
+//Negative numbers deletion
+for(let i = 0; i < new_scores.length; i++){
+  const value = new_scores[i]
+  if(value < 0){
+    new_scores.splice(i,1) //delete element in position i
+    i--
+    console.log(new_scores)
   }
 }
 
-for (const c of [1,2]) {
-  const smallest = Math.min(...newscores)
-  const pos_smallest = newscores.indexOf(smallest)
-  console.log("Smallest", smallest, "at position", pos_smallest)
-  console.log(`The smallest value is ${smallest} and is at position ${pos_smallest}`)
-  newscores.splice(pos_smallest,1)
+
+//Smallests delition
+for(let i = 0; i<2; i++){
+  const smallest = Math.min(...new_scores)
+  const indexSmallest = new_scores.indexOf(smallest)
+  new_scores.splice(indexSmallest,1)
+  console.log(new_scores)
 }
+
+//Adding elements
 
 let avg = 0.0
-for (const value of newscores) {
+
+for(const value of new_scores){
   avg += value
 }
-avg /= newscores.length
+avg = avg / new_scores.length
 avg = Math.round(avg)
 
-console.log(avg)
+/* NO (per inserire NN+2 avg)
+for(let i = 0; i< scores.length - new_scores.length; i++){ //condition evaluated at every iteration!!
+  new_scores.push(avg)
+  console.log(scores)
+  console.log(new_scores)
+}
+*/
 
-while( scores.length > newscores.length) {
-  newscores.push(avg)
+while(scores.length > new_scores.length){
+  new_scores.push(avg)
+  console.log(scores)
+  console.log(new_scores)
 }
 
 
-// const newscores = [...scores]  // make a copy
+let avgScore=0
+let avgNew=0
 
-// for (let i = 0 ; i<newscores.length; i++ ) {
-//   const value = newscores[i]
-//   if(value<0) {
-//     newscores.splice(i, 1)
-//   }
-//   console.log(value)
-// }
-console.log(scores)
-console.log(newscores)
+for(let i = 0; i<scores.length; i++){
+  avgScore+=scores[i]
+  avgNew+=new_scores[i]
+}
 
-// console.log(scores)
+avgScore = avgScore / scores.length
+avgNew = avgNew / new_scores.length
 
+avgScore = Math.round(avgScore)
+console.log(`AVG SCORES:${avgScore}`)
+
+avgNew = Math.round(avgNew)
+console.log(`AVG SCORES:${avgNew}`)
--- week01/my-users.js
+++ week01/my-users.js
@@ -29,4 +29,19 @@ for( const name of list_names )
 
 
 
+const list_names = names.split(",")
+for (let i = 0; i<list_names.length; i++) {
+  //name = name.trim() //modifies the reference
+  list_names[i] = list_names[i].trim()
+}
+console.log(list_names)
 console.log(names)
+const acronyms = []
+for(let i = 0; i<list_names.length; i++){
+  const tmp_list = list_names[i].split(" ")
+  for(let j=0; j<tmp_list.length; j++){
+    tmp_list[j] = tmp_list[j][0]
+  }
+  acronyms.push(tmp_list.join(""))
+}
+console.log(acronyms.sort())
