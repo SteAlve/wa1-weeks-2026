121e87b2352cfe9a3e3b27ea64bb22ffb25c7cf0
Alverino Stefano s354116 <s312200@studenti.polito.it>
2026-02-27 11:13:50 +0100

    feat: finished exercise 1

--- week01/exercise1/exercise1.js
+++ week01/exercise1/exercise1.js
@@ -23,14 +23,33 @@ function Question (text, userId, date) {
         }
         return tmpAns;
     }
+    this.afterDate = (date) => {
+        const tmpAns = [];
+        for(const ans of this.answers){
+            if(ans.date.isAfter(date)){
+                tmpAns.push(ans);
+            }
+        }
+        return tmpAns;
+    }
 }
 
 const q1 = new Question("How long is this exercise?", 1, "2026-02-27");
 const a1 = new Answer("too much", 2, "2026-02-27");
 const a2 = new Answer("10 minutes", 3, "2026-02-27");
-console.log(q1);
+const a3 = new Answer("a day", 3, "2026-02-27");
+const a4 = new Answer("30 minutes", 3, "2026-02-26");
+//console.log(q1);
 q1.addAnswer(a1);
-console.log(q1);
+q1.addAnswer(a2);
+q1.addAnswer(a3);
+q1.addAnswer(a4);
+//console.log(q1);
 const user2a = q1.getAnswers(2);
+const user3a = q1.getAnswers(3);
 
-console.log(user2a);
+//console.log(user2a);
+//console.log(user3a);
+
+const answersAfterYesterday = q1.afterDate(dayjs("2026-02-26"));
+console.log(answersAfterYesterday);
